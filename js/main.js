af9faea100193d75063765ca4eb985972f4d58dc
Simbarashe Mupfukudzwa <simba3310@gmail.com>
2018-02-20 18:01:30 -0600

    new functions

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/*jshint esversion:6 */
+let origBoard;
+const huPlayer ='O';
+const aiPlayer ='X';
+const winCombos = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+];
+
+const cells = document.querySelectorAll('.cell');
+const startGame = ()=>{
+    let endGame = document.querySelector(".endgame");
+    endGame.style.display="none";
+    origBoard = Array.from(Array(9).keys());
+};
+
+startGame();
+
